2ec257001453af54b2c73d2e89e5e374e03025c3
euclarhxz <160048272+euclarhxz@users.noreply.github.com>
2024-09-18 14:57:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,46 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Historia',
+    'Onde surgiram os primeiros ancestrais do homem?',
+    'Pelo que sabemos, os humanos surgiram na África Oriental há cerca de 2,5 milhões de anos, no final do Plioceno, período que compreende de 5 a 2 milhões atrás. '
+)
+
+criaCartao(
+    'Portugues',
+    'Quem foi a primeira pessoa a viajar no Espaço?',
+    'Yuri Gagarin'
+)
+criaCartao(
+    'Portugues',
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest'
+)
+
+criaCartao(
+    'Portugues',
+    'Onde se localiza Machu Picchu? ',
+    ''
+)
+
